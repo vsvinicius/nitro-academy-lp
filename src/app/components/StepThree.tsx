2abf641f921104ebb53a23539e7e9cd2c0ac94f9
Vinicius da Silva <vinicius.svsilva@gmail.com>
2025-03-04 20:10:18 -0300

    updates alt

--- src/app/components/StepThree.tsx
+++ src/app/components/StepThree.tsx
@@ -8,11 +8,11 @@ export default function StepThree() {
         <p className='w-80 pt-5'>Escolha suas matérias a cada semestre</p>
       </div>
       <div>
-        <Image src="target.svg" width={75} height={75} alt="icone correto" className='w-20 h-20' />
+        <Image src="target.svg" width={75} height={75} alt="icone flecha ao alvo" className='w-20 h-20' />
         <p className='w-80 pt-5'>Aprenda o que realmente interessa</p>
       </div>
       <div>
-        <Image src="online-class.svg" width={75} height={75} alt="icone correto" className='w-20 h-20' />
+        <Image src="online-class.svg" width={75} height={75} alt="icone aula online" className='w-20 h-20' />
         <p className='w-80 pt-5'>Aulas 100% online, interativas e ao vivo</p>
       </div>
 
