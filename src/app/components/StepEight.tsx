1aa21484b7361ad3d4fa443b9d5ab89104e78ca7
Thyelen Engel <130775295+thyengel@users.noreply.github.com>
2025-03-04 22:04:40 -0300

    adds step eight (#10)
    * adds step eight
    
    * add title.map

---
+++ src/app/components/StepEight.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import { Fragment } from "react";
+
+const titles = [
+  "Turmas reduzidas (máximo 10 alunos)",
+  "Ensino personalizado",
+  "Desenvolvimento de habilidades do século XXI",
+  "Flexibilidade de horários",
+  "Professores de alto nível",
+  "Uso produtivo do tempo livre",
+];
+
+export default function StepEight() {
+  return (
+    <div className="h-[85rem] px-12">
+      <h1 className="text-theme-orange font-gilroy-extrabold text-6xl pl-4 pr-6">
+        Por que escolher a Nitro?
+      </h1>
+      {titles.map((title) => (
+        <Fragment key={title}>
+          <Image
+            src="logo-azul.png"
+            alt="Logo Nitro"
+            width={64}
+            height={64}
+            className="pt-10"
+          />
+          <p className="font-montserrat-regular text-2xl pl-1 pr-5 pt-4">
+            {title}
+          </p>
+        </Fragment>
+      ))}
+    </div>
+  );
+}
